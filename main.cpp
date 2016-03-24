4ab8ead5d28a2152db1a9bb5b0b7f3537eb3ddf7
astapov-k-a <akam.softally@gmail.com>
2016-03-24 09:53:28 +0300

    Update main.cpp
    full test

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+///@file test file
+///@brief only for test
 ///@author Astapov K.A. +7(931)-29-17-0-16
 ///@date 22/03/2016
 
@@ -7,14 +9,37 @@
 
 
 class F{};
-class D : public example_lib::Base {};
+class D : public example_lib::Base {
+ public:
+  D() : x(56635) {}
+  int x;
+};
 
 class E{
  public:
+  E() : x(255) {}
   ~E() {std::wcout << L"\nE destruct";}
+  int x;
 };
-class G : public example_lib::Base { public:  ~G() {std::wcout << L"\nG destruct";} };
+class G : public example_lib::Base {
+ public:
+ G() : x(65535) {}
+ int x;
+ ~G() {std::wcout << L"\nG destruct";}
+};
 
+template <typename Tn> void Test(example_lib::SmartPointer<Tn> ptr) {
+  Tn a = *ptr;
+  Tn c;
+  //c. x=(17);
+  example_lib::SmartPointer<Tn> ptr2 = ptr;
+  const Tn * b =ptr.operator ->();
+  a = *b;
+  *ptr2 = c;
+  Q_UNUSED(a);
+  Q_UNUSED(b);
+  Q_UNUSED(c);
+}
 int main(int argc, char *argv[])
 {
   volatile int x = 0;
@@ -35,10 +60,19 @@ int main(int argc, char *argv[])
   example_lib::SmartPointer<int> ptr1 = example_lib::Make<int>();
   example_lib::SmartPointer<E  > ptr2 = example_lib::Make<E  >();
   example_lib::SmartPointer<G  > ptr3 = example_lib::Make<G  >();
+  Test(ptr1);
+  Test(ptr2);
+  Test(ptr3);
+  E to_copy_e;
+  G to_copy_g;
+  to_copy_e.x = 17;
+  to_copy_g.x = 18;
+  *ptr2 = to_copy_e;
+  *ptr3 = to_copy_g;
   std::wcout<<L"\n\nFun Test destruction done\n\n";
   //example_lib::SmartPointer<Size> xx;
   //example_lib::internal::FoeOrSimpleControl<Size> t;
-  QCoreApplication a(argc, argv);
+  //QCoreApplication a(argc, argv);
     
-    return a.exec();
+  //return a.exec();
 }
